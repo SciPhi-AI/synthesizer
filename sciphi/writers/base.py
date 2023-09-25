a4cca37eb487d435ed94476b45bfd649ce17aef9
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2023-09-25 16:54:09 -0400

    Feature/slim configs rebased (#32)
    * Add back grade school
    
    * slim configs

--- sciphi/writers/base.py
+++ sciphi/writers/base.py
@@ -1,4 +1,6 @@
 """A module which defines the abstract bata class for data writers."""
+import os
+import time
 from abc import ABC, abstractmethod
 
 
@@ -7,6 +9,13 @@ class DataWriter(ABC):
 
     def __init__(self, output_path):
         self.output_path = output_path
+
+    def _get_modified_path(self):
+        """Modify the output path if the file already exists and overwriting is not allowed."""
+        if not self.overwrite and os.path.exists(self.output_path):
+            base_name, ext = os.path.splitext(self.output_path)
+            return f"{base_name}_{int(time.time())}{ext}"
+        return self.output_path
 
     @abstractmethod
     def write(self, data):
