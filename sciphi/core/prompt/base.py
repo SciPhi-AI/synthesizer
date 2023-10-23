4026f405ae5e8864e56f1a60d8a546da46d94762
Owen <ocolegro@gmail.com>
2023-10-23 19:49:24 -0400

    Adding a workflow for data augmentation

--- sciphi/core/prompt/base.py
+++ sciphi/core/prompt/base.py
@@ -1,46 +1,84 @@
 """A module to facilitate seamless construction of input prompts"""
+import os
 from dataclasses import dataclass
-from enum import Enum
-from typing import Optional, Union
+from typing import Optional
 
-
-class PromptStructure(Enum):
-    """How are prompts formatted (e.g. single reply, conversation, ..)"""
-
-    SINGLE = "single"
-    CONVERSATION = "conversation"
+import yaml
 
 
 @dataclass
 class Prompt:
     """A simple abstraction for building input prompts."""
 
-    expected_inputs: set[str]
-    raw_text: Union[str, list[str]]
-    structure: PromptStructure
-    _text: Optional[Union[str, list[str]]] = None
+    config: dict
 
-    def format(self, **kwargs) -> None:
+    def __init__(
+        self, config: Optional[dict] = None, config_path: Optional[dict] = None
+    ) -> None:
+        if config and config_path or not config and not config_path:
+            raise ValueError(
+                "Must provide either a config or a config path, but not both."
+            )
+        if not os.path.exists(config_path):
+            raise ValueError(f"Config path {config_path} does not exist.")
+        if config_path:
+            with open(config_path, "r") as yaml_file:
+                config = yaml.safe_load(yaml_file)
+        if "raw_text" not in config:
+            raise ValueError("Prompt config must contain a `raw_text` field.")
+        if "dataset_supplied_inputs_map" not in config:
+            raise ValueError(
+                "Prompt config must contain a `dataset_supplied_inputs_map` field."
+            )
+        if "user_supplied_inputs" not in config:
+            raise ValueError(
+                "Prompt config must contain a `user_supplied_inputs` field."
+            )
+
+        self.user_supplied_inputs = config["user_supplied_inputs"]
+        self.dataset_supplied_inputs_map = config[
+            "dataset_supplied_inputs_map"
+        ]
+        self.dataset_expected_inputs = list(
+            config["dataset_supplied_inputs_map"].values()
+        )
+        self.dataset_expected_columns = list(
+            config["dataset_supplied_inputs_map"].keys()
+        )
+        if set(self.user_supplied_inputs).intersection(
+            self.dataset_expected_inputs
+        ):
+            raise ValueError(
+                "Prompt config cannot overlap `user_supplied_inputs` and `dataset_supplied_inputs_map` values."
+            )
+        self.expected_inputs = set(
+            self.user_supplied_inputs + self.dataset_expected_inputs
+        )
+        self.raw_text = config["raw_text"]
+
+    def format(self, dataset_entry: Optional[dict] = None, **kwargs) -> str:
         """Format the prompt into a string"""
+        if self.dataset_expected_columns and not dataset_entry:
+            raise ValueError(
+                "Expected a dataset entry, but none was provided."
+            )
+        for key in self.dataset_expected_columns:
+            if key not in dataset_entry:
+                raise ValueError(
+                    f"Expected dataset entry to contain {key}, but it was not found."
+                )
+            kwargs[self.dataset_supplied_inputs_map[key]] = dataset_entry[key]
+
         if set(kwargs.keys()) != self.expected_inputs:
             raise ValueError(
                 f"Received {kwargs.keys()}, but expected {self.expected_inputs}"
             )
-        if self.structure == PromptStructure.SINGLE:
-            if not isinstance(self.raw_text, str):
-                raise ValueError(
-                    f"Expected `raw_text` to be a string, but got {type(self.raw_text)}"
-                )
-            self._text = self.raw_text.format(**kwargs)
-        else:
-            if not isinstance(self.raw_text, list) and all(
-                isinstance(item, str) for item in self.raw_text
-            ):
-                raise ValueError(
-                    f"Expected `raw_text` to be a list of strings, but got {type(self.raw_text)}"
-                )
-            # If not a single response, then text is formatted in a list
-            self._text = [ele.format(**kwargs) for ele in self.raw_text]
+        if not isinstance(self.raw_text, str):
+            raise ValueError(
+                f"Expected `raw_text` to be a string, but got {type(self.raw_text)}"
+            )
+        print("kwargs = ", kwargs)
+        return self.raw_text.format(**kwargs)
 
     @property
     def text(self):
@@ -50,33 +88,6 @@ class Prompt:
             )
         return self._text
 
-
-class PromptManager:
-    """A manager for available prompts"""
-
-    stock_prompts = {
-        "md_instruction": Prompt(
-            expected_inputs={"instruction"},
-            raw_text="""Below is an instruction that describes a task. Write a response that appropriately completes the request.\n### Instruction:\n{instruction}\n\n### Response:""",
-            structure=PromptStructure.SINGLE,
-        ),
-        "unit": Prompt(
-            expected_inputs={"instruction"},
-            raw_text="""{instruction}""",
-            structure=PromptStructure.SINGLE,
-        ),
-    }
-
-    def __init__(self, stock_prompts=stock_prompts) -> None:
-        self.stock_prompts = stock_prompts
-
-    def get_prompt(self, selected_prompt: str) -> Prompt:
-        """
-        Gets the list of prompts associated with the specified mode
-        """
-        prompt = self.stock_prompts.get(selected_prompt)
-        if not prompt:
-            raise ValueError(
-                f"Requested prompt mode {selected_prompt} does not exist!"
-            )
-        return prompt
+    # @property
+    # def required_data_fields(self):
+    #     return self.config["dataset_supplied_inputs"].keys()
